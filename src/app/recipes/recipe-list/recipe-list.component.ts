a2e9fc83d9ea39baba724f4c9f361d1d9bb87920
Malix_55 <malikzabdullah@gmail.com>
2023-01-06 16:48:56 +0500

    Basic application setup and components added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent { 
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'this is simply a test', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-2.jpg.webp'),
+    new Recipe('A test Recipe','this is simply a test','https://www.indianhealthyrecipes.com/wp-content/uploads/2015/10/pizza-recipe-2.jpg.webp')
+  ];
 
-}
+} 
